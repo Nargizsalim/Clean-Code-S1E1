95a96ce6b54f931b991a516ffa902aa170553ef3
Nargiz Salimova <nargiz.salimova@gmail.com>
2022-10-27 11:00:03 +0600

    refactor: add BEM notation according to rule 2.1 in html-and-css.md

--- app.js
+++ app.js
@@ -10,8 +10,8 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var incompleteTaskHolder=document.querySelector(".main__incomplete_list");//ul of #incompleteTasks
+var completedTasksHolder=document.querySelector(".main__complete_list");//completed-tasks
 
 
 //New task list item
